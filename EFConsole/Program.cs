1cc05490c09bf087a1f9c7993eae8223237d9b7d
jungmtg <cardofang@gmail.com>
2016-05-14 15:26:57 +0800

    Native Sql

--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -7,24 +7,39 @@ using System.Threading.Tasks;
 
 namespace EFConsole
 {
+
+	public  class DeptCourseCount
+	{
+		public int DepartmentID { get; set; }
+		public string Name { get; set; }
+		public int CourseCount { get; set; }
+	}
+
 	class Program
 	{
+		
+
 		static void Main(string[] args)
 		{
 			
 			using (var db = new ContosoUniversityEntities())
 			{
-				foreach (var dept in db.Department)
-				{
-					Console.WriteLine(dept.Name);
-					foreach (var course in dept.Course)
+				
+				var sql = @"SELECT Department.DepartmentID,
+						          Department.Name, 
+	                              COUNT(*) AS CourseCount
+							      FROM Course LEFT OUTER JOIN
+								  Department ON Course.DepartmentID = Department.DepartmentID
+							      GROUP BY Department.DepartmentID, Department.Name";
+
+				var data = db.Database.SqlQuery<DeptCourseCount>(sql);
+
+					foreach (var item in data)
 					{
-						Console.WriteLine("\t"+course.Title);
+						Console.WriteLine(item.Name+"\t"+item.CourseCount);
 					}
 
-					Console.ReadKey();
-
-				}
+				Console.ReadKey();
 
 
 			}
