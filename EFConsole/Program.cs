0dc821a94dc6a455acd907472e97f2a69920bc3b
jungmtg <cardofang@gmail.com>
2016-05-14 11:01:41 +0800

    Insert

--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -10,8 +10,46 @@ namespace EFConsole
 	{
 		static void Main(string[] args)
 		{
+			using (var db = new ContosoUniversityEntities())
+			{
+				//foreach (var item in db.Course)
+				//{
+					//Console.WriteLine(item.Department.Name);//String Types
 
-			
+				//}
+				var data = from p in db.Course
+						   where p.Title.Contains("Git")
+					select p;
+				foreach (var item in data)
+				{
+					Console.WriteLine(item.Title);
+				}
+
+				//var c =db.Course.Add(new Course()
+				//{
+				//	Title ="Git Test2",
+				//	Credits = 5,
+				//	//DepartmentID = 1
+				//});
+
+				var c = new Course()
+				{
+					Title = "Git Test 2",
+					Credits = 4
+				};
+
+				c.Department = db.Department.Find(2);
+
+				db.SaveChanges();
+
+
+				foreach (var item in db.Course.ToList())
+				{
+					Console.WriteLine(item.Title);
+				}
+
+			}
+
 
 
 		}
