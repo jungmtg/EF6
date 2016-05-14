ef19cef6e3cd659e737b5f3063e600d5f139470e
jungmtg <cardofang@gmail.com>
2016-05-14 09:58:03 +0800

    init

---
+++ EFConsole/Class1.partial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsole
+{
+	public partial class Class1
+	{
+		partial void Initialize();
+	}
+}
---
+++ EFConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			#region
+			//string a = null;
+
+			//string b = a ?? "test";
+			//syntax sugar
+			//if (a == null) b = "test";
+			//else
+			//{
+			//	b = a;
+			//}
+			//C#6.0
+			//int? c = a?.Length.ToString();
+			#endregion
+
+
+		}
+	}
+}
