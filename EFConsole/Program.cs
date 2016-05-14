7d8ec1af31a470eed578cb8fd4442ca224b4b8a2
jungmtg <cardofang@gmail.com>
2016-05-14 11:21:14 +0800

    delete dadta

--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -16,33 +16,13 @@ namespace EFConsole
 				var data = from p in db.Course
 						   where p.Title.Contains("Git")
 					select p;
-				//foreach (var item in data)
-				//{
-				//	Console.WriteLine(item.Title);
-				//}
+				
 
-				//var c = new Course()
-				//{
-				//	Title = "Git Test 2",
-				//	Credits = 4
-				//};
-
-				//c.Department = db.Department.Find(2);
-				//db.Course.Add(c);
-
-				//Console.WriteLine(c.CourseID);
-				//db.SaveChanges();
-				//Console.WriteLine(c.CourseID);
-				//Console.ReadKey();
+				
 
 				var c8 = db.Course.Find(8);
-				#region False Example
-				//This Data is not exist in current db
-				//new Course() { CourseID = 12}
-				#endregion
-				db.Course.Remove(
-					c8
-					);
+				
+				db.Course.Remove(c8);
 				db.SaveChanges();
 
 			}
