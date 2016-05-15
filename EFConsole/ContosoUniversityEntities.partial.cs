d78a9d3f94589cdd98d9ac784c49992738dce1a8
jungmtg <cardofang@gmail.com>
2016-05-15 10:23:19 +0800

    add ContosoUniversityEntities partial class override savechanges

---
+++ EFConsole/ContosoUniversityEntities.partial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsole
+{
+	public partial class ContosoUniversityEntities:DbContext
+	{
+		public override int SaveChanges()
+		{
+			var entries = this.ChangeTracker.Entries();
+			foreach (var entity  in entries)
+			{
+				if (entity.Entity is Course) //entity.Entity 是Course型別 或祖先
+				{
+					//var c = (Course) entity.Entity;
+					//c.ModifiedOn=DateTime.Now;
+					
+				entity.CurrentValues.SetValues(
+					new{
+						ModifiedOn = DateTime.Now
+					}
+				);
+				}
+			}
+			return base.SaveChanges();
+		}
+	}
+}
