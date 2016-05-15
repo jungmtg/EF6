4f7a9759c0603d7c91af9804306564953a30fe0b
jungmtg <cardofang@gmail.com>
2016-05-15 13:09:45 +0800

    Concurrency

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("APP1");
+			using (var db = new ContosoUniversityEntities())
+			{
+				db.Database.Log = Console.WriteLine;
+
+				var c = db.Department.Find(1);
+				c.Name += "XX";
+				Console.ReadLine();
+				try
+				{
+					db.SaveChanges();
+				}
+				catch (DbUpdateConcurrencyException ex)
+				{
+					throw;
+				}
+			}
+		}
+	}
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("APP2");
+			using (var db = new ContosoUniversityEntities())
+			{
+				db.Database.Log = Console.WriteLine;
+
+				var c = db.Department.Find(1);
+				c.Name += "**";
+				Console.ReadLine();
+				try
+				{
+					db.SaveChanges();
+				}
+				catch (DbUpdateConcurrencyException ex)
+				{
+					throw;
+				}
+			}
+		}
+	}
+}
